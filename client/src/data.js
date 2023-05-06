074bd70ba134636cb366ffaa8c90c59dcd6af12f
dhruv gupta <dhruv.gupta@credextechnology.com>
2023-05-06 14:05:24 +0530

    first

---
+++ client/src/data.js
@@ -0,0 +1,88 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://media.gq.com/photos/5798cf943a803448479c3caf/16:9/w_2560%2Cc_limit/ana-de-armas-gq-0816.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://static.photocdn.pt/images/articles/2019/07/24/images/articles/2019/07/22/best_photo_poses_for_men-1.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.squarespace-cdn.com/content/v1/58019f4f6b8f5b28658aa49b/1511757396582-1R9GTYAM9AGSW8NTBMYY/Heights-Studio-Family-Portraits-3.jpg?format=1000w",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "WOMEN",
+      cat:"women"
+    },
+    {
+      id: 2,
+      img: "https://static.giga.de/wp-content/uploads/2022/02/sigma-male-GettyImages-1201023261-rcm1600x1200u.jpg",
+      title: "MEN",
+      cat:"men"
+    },
+    {
+      id: 3,
+      img: "https://zigverve.com/wp-content/uploads/2020/03/Women-Accessories-Featured-Image.jpg",
+      title: "ACCESSORIES",
+      cat:"accessories"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/h/z/h/s-striped-men-polo-neck-multicolor-t-shirt-pack-of-1-inkkr-original-imaggzygghgsftu4.jpeg?q=70",
+    },
+    {
+      id:2,
+      img:"https://rukminim1.flixcart.com/image/832/832/l4zxn680/t-shirt/6/f/u/s-hood-260-checkedshirt-white-lewel-original-imagfryhczbhtjfh.jpeg?q=70",
+    },
+    {
+      id:3,
+      img:"https://rukminim1.flixcart.com/image/832/832/kwl0akw0/t-shirt/n/z/g/l-jc21-rn-fs-navy-grey-spray-aop-jump-cuts-original-imag98etctdvzzha.jpeg?q=70",
+    },
+    {
+      id:4,
+      img:"https://rukminim1.flixcart.com/image/832/832/xif0q/kurta/z/q/l/m-kast110md-majestic-man-original-imafw49uamqn455g-bb.jpeg?q=70",
+    },
+    {
+      id:5,
+      img:"https://rukminim1.flixcart.com/image/832/832/kf1fo280hlty2aw-0/jacket/4/j/p/l-1-hljk000138-highlander-original-imafvna2fzzacyuz.jpeg?q=70",
+    },
+    {
+      id:6,
+      img:"https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/v/u/n/s-kwt-48-fsbxyft-wt-killer-original-imaghbj5aqzttxq6.jpeg?q=70",
+    },
+    {
+      id:7,
+      img:"https://rukminim1.flixcart.com/image/832/832/kflftzk0/top/h/e/f/s-gr5872-rust-harpa-original-imafwyshr3trn97j.jpeg?q=70",
+    },
+    {
+      id:8,
+      img:"https://rukminim1.flixcart.com/image/832/832/ke0a7ww0/top/b/b/s/s-gr6027-pink-harpa-original-imafusf3t3gdxhnz.jpeg?q=70",
+    },
+    {
+      id:9,
+      img:"https://rukminim1.flixcart.com/image/832/832/kx9as280/top/p/b/0/-original-imag9r2crfpxqtx2.jpeg?q=70",
+    },
+    {
+      id:10,
+      img:"https://rukminim1.flixcart.com/image/832/832/kyhlfgw0/top/9/j/f/-original-imagapzwstwwctz8.jpeg?q=70",
+    },
+  ]
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1462758295552-26c127a355c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+  opacity: 0.9;
+  ${mobile({ width: "75%" })}
+
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+  margin-left: 110px;
+  ${mobile({ marginLeft: "85px;" })}
+
+  
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+  
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Button>LOGIN</Button>
+          <Link>FORGOT PASSWORD</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
